4064121ab2dfc655b188e57e741c0f630607e6d4
guibalsi1 <guilhermebalsi@gmail.com>
2025-10-01 22:16:47 -0300

    :test_tube: test (DeliveryApiAplication) Testando aplicação

--- src/main/java/com/deliverytech/delivery/DeliveryApiApplication.java
+++ src/main/java/com/deliverytech/delivery/DeliveryApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DeliveryApiApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Delivery API Application started");
 		SpringApplication.run(DeliveryApiApplication.class, args);
 	}
 
